7a6e720704c7a4302b24b7395079c0810c9bbf2d
DIEGO GARCIA VAZQUEZ <diego.garcia.vazquez@bbva.com>
2022-08-23 17:44:34 -0500

    Creacion de clase que enviara mensajes automaticos al servicio

---
+++ src/com/dinobotica/streams/AutoClient.java
@@ -0,0 +1,23 @@
+package com.dinobotica.streams;
+
+import java.io.IOException;
+
+public class AutoClient implements Runnable{
+    
+    
+    public AutoClient(String server, int port) throws IOException
+    {
+        
+    }
+
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+    
+    
+}
